a2d647f12b1b6db92c7afaf1730edcf5e47c9e1f
Tim Davey <timjdavey@tims-mbp.home>
2021-06-29 16:41:05 +0100

    cleaning up hist plots

--- helpers/plots.py
+++ helpers/plots.py
@@ -19,22 +19,24 @@ def dual(observations, bins, labels=None,
 
 
     # Subplots
-    fig, axes = plt.subplots(1, 2, sharex=True, sharey=False, figsize=(15,5))
+    fig, axes = plt.subplots(1, 2, sharex=True, sharey=True, figsize=(15,5))
     
     # Ensembles
     axes[0].set_title('Distributions by %s' % variable)
     # Colours - reverse colours
     legend = labels is not None and len(labels) < 12
     g = sns.histplot(ax=axes[0],
-        bins=bins, element='step', fill=True, stat='density',
-        data=tidy_ensembles, x=tidy_value, hue=tidy_variable,
-        palette="%s_r" % palette, alpha=0.3, legend=legend)
+        data=tidy_ensembles, bins=bins,
+        x=tidy_value, hue=tidy_variable,
+        element='step', fill=True, stat='probability',
+        common_norm=False, multiple='dodge',
+        palette=palette, alpha=0.2, legend=legend)
 
     # Ergodic
     axes[1].set_title('Distribution of all observations (ergodic)')
     g = sns.histplot(ax=axes[1],
-        bins=bins, element='step', stat='density',
         data=tidy_ergo, x=tidy_value, hue=tidy_variable,
+        bins=bins, element='step', stat='probability',
         palette=palette, alpha=1.0, legend=False)
 
 
@@ -55,14 +57,14 @@ def ridge(observations, bins, tidy_variable='variable',
 
         # Initialize the FacetGrid object
         g = sns.FacetGrid(tidy_ensembles, row=tidy_variable,
-            hue=tidy_variable, aspect=5, height=1.2, palette="%s_r" % palette)
+            hue=tidy_variable, aspect=5, height=0.8, palette=palette)
         
         g.map(sns.histplot, tidy_value,
-            element='step', bins=bins,
-            fill=True, alpha=0.7)
+            element='step', bins=bins, stat='probability',
+            fill=True, alpha=0.6, common_norm=False)
     
         # Set the subplots to overlap
-        g.fig.subplots_adjust(hspace=-0.5)
+        g.fig.subplots_adjust(hspace=-0.6)
         # Remove axes details that don't play well with overlap
         g.fig.suptitle(title)
         g.set_titles("")
